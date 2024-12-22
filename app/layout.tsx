5e7bffc3090608c668530ef333cce7915bfefd3b
Amanuel Chaka <amigojr2030@gmail.com>
2024-12-22 19:32:32 +0300

    ts error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Ubuntu } from "next/font/google";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Ubuntu } from 'next/font/google';
 
 const ubuntu = Ubuntu({
-  weight: ["300", "400", "500", "700"],
-  display: "swap",
+  weight: ['300', '400', '500', '700'],
+  display: 'swap',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "VerseHub",
+  title: 'VerseHub',
   description:
-    "VerseHub is a comprehensive platform dedicated to providing an extensive collection of Bible verses categorized by themes, offering users a rich and meaningful experience.",
+    'VerseHub is a comprehensive platform dedicated to providing an extensive collection of Bible verses categorized by themes, offering users a rich and meaningful experience.',
 };
 
 export default function RootLayout({
@@ -19,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${ubuntu.className} antialiased`}>{children}</body>
     </html>
   );
--- app/verse/[id]/page.tsx
+++ app/verse/[id]/page.tsx
@@ -1,16 +1,17 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { verses } from "@/data/verses";
-import { ChevronRight, ChevronLeft } from "lucide-react";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { verses } from '@/data/verses';
+import { ChevronRight, ChevronLeft } from 'lucide-react';
 
 export default function VersePage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [verse, setVerse] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
@@ -62,51 +63,51 @@ export default function VersePage({
 
   return (
     <>
-      <div className="min-h-screen p-8 flex flex-col items-center justify-center">
-        <div className="bg-white p-8 border-4 border-black shadow-[12px_12px_0_0_#000] max-w-2xl w-full">
-          <h1 className="text-4xl font-bold mb-8 text-center">
+      <div className='min-h-screen p-8 flex flex-col items-center justify-center'>
+        <div className='bg-white p-8 border-4 border-black shadow-[12px_12px_0_0_#000] max-w-2xl w-full'>
+          <h1 className='text-4xl font-bold mb-8 text-center'>
             {verse.reference}
           </h1>
-          <p className="md:text-2xl mb-8">{verse.text}</p>
-          <div className="flex flex-col gap-3 md:justify-between md:items-center md:flex-row md:gap-0">
+          <p className='md:text-2xl mb-8'>{verse.text}</p>
+          <div className='flex flex-col gap-3 md:justify-between md:items-center md:flex-row md:gap-0'>
             <Link
               href={`/category/${verse.category.toLowerCase()}`}
-              className="bg-black text-white text-center px-4 py-2 font-bold hover:bg-gray-800 transition-colors duration-300"
+              className='bg-black text-white text-center px-4 py-2 font-bold hover:bg-gray-800 transition-colors duration-300'
             >
               More {verse.category} Verses
             </Link>
             <button
               onClick={copyToClipboard}
-              className="bg-black text-white px-4 py-2 font-bold hover:bg-gray-800 transition-colors duration-300"
+              className='bg-black text-white px-4 py-2 font-bold hover:bg-gray-800 transition-colors duration-300'
             >
-              {copied ? "Copied!" : "Copy Verse"}
+              {copied ? 'Copied!' : 'Copy Verse'}
             </button>
           </div>
-          <div className="flex items-center justify-center mt-6">
+          <div className='flex items-center justify-center mt-6'>
             <ChevronLeft
               size={60}
-              color="black"
-              className="cursor-pointer p-2"
+              color='black'
+              className='cursor-pointer p-2'
               onClick={previousVerse}
             />
             <ChevronRight
               size={60}
-              color="black"
-              className="cursor-pointer p-2"
+              color='black'
+              className='cursor-pointer p-2'
               onClick={nextVerse}
             />
           </div>
         </div>
         <Link
-          href="/"
-          className="mt-8 bg-black text-white px-6 py-3 font-bold hover:bg-gray-800 transition-colors duration-300"
+          href='/'
+          className='mt-8 bg-black text-white px-6 py-3 font-bold hover:bg-gray-800 transition-colors duration-300'
         >
           Back to Home
         </Link>
       </div>
 
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="fixed bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      <div className='absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]'>
+        <div className='fixed bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]'></div>
       </div>
     </>
   );
